ebed552753f514046aaa0ea36c0c0424bb22e348
Matt Villwock <mtvillwock@gmail.com>
2017-06-30 15:08:16 -0700

    Add initial props example

--- src/App.js
+++ src/App.js
@@ -6,14 +6,21 @@ class App extends React.Component {
   render() {
     // return React.createElement('h1', null, 'Hello Eggheads');
     // Null is where Props would be passed
+    let text = this.props.text;
     return (
+
       <div>
         <h1>Class Component</h1>
         <h2>Subtitle</h2>
-        <p>Description paragraph</p>
+        <p>Description paragraph: {text}</p>
       </div>
     );
   }
+}
+
+App.propsTypes = {
+  text: React.PropTypes.string.isRequired,
+  number: React.PropTypes.number.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
+  <App text="Props text here" number={5} />,
   document.getElementById('root')
 );
 registerServiceWorker();
