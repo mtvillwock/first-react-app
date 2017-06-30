d4737641373ac9bba54e493a9852ce46f3a0f083
Matt Villwock <mtvillwock@gmail.com>
2017-06-30 15:09:35 -0700

    Add state attr example

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import React, { Component } from 'react';
 // const App = () => <h1>Hello Stateless</h1>
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      foo: "Foo state text"
+    }
+  }
+
   render() {
     // return React.createElement('h1', null, 'Hello Eggheads');
     // Null is where Props would be passed
@@ -13,6 +21,7 @@ class App extends React.Component {
         <h1>Class Component</h1>
         <h2>Subtitle</h2>
         <p>Description paragraph: {text}</p>
+        <p>{this.state.foo}</p>
       </div>
     );
   }
