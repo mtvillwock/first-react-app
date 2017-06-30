7a59b5d583170341ae8d6739cf89c5e310bbec0b
Matt Villwock <mtvillwock@gmail.com>
2017-06-30 15:53:25 -0700

    Update and clean up on unmount

--- src/App.js
+++ src/App.js
@@ -17,15 +17,22 @@ class App extends React.Component {
 
   componentWillMount() {
     console.log("componentWillMount")
+    this.setState({m: 2})
   }
 
   render() {
     console.log("rendered")
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
 
   componentDidMount() {
     console.log('componentDidMount')
+    this.inc = setInterval(this.update, 500)
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount')
+    clearInterval(this.inc)
   }
 }
 
