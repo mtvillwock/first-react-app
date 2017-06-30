c26142dbccda4e53648eb197f1d169fc51385d70
Matt Villwock <mtvillwock@gmail.com>
2017-06-30 16:38:54 -0700

    Use higher order components to allow different components to use similar behaviors differently
    E.g., update on click vs on mouse

--- src/App.js
+++ src/App.js
@@ -2,44 +2,79 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 // const App = () => <h1>Hello Stateless</h1>
 
+// Share common functionality between components
+// Takes in a component and returns a new component
+// Replaces mixins?
+// E.g., build components to specification instead of including mixins all over
+// ??? Not 100% sure how this differs from mixins... more explicit, I guess?
+const HigherOrderComponent = (InnerComponent) => class extends React.Component {
+  constructor() {
+    super()
+
+    this.state = { count: 0 }
+  }
+
+  componentWillMount() {
+    console.log("HigherOrderComponent will mount")
+  }
+
+  update() {
+    this.setState({count: this.state.count+1})
+  }
+
+  render() {
+    return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    )
+  }
+}
+
+
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = { items: [] }
-  }
-
-  filter(e) {
-    this.setState({ filter: e.target.value })
-  }
-
-  componentWillMount() {
-    fetch('http://swapi.co/api/people/?format=json')
-      .then( response => response.json() )
-      .then( ({results: items}) => this.setState({items}) )
   }
 
   render() {
-    let items = this.state.items
-    if(this.state.filter) {
-      items = items.filter( item =>
-        item.name.toLowerCase()
-          .includes(this.state.filter.toLowerCase())
-        )
-    }
-
     return(
       <div>
-        <input type="text"
-          onChange={this.filter.bind(this)}
-        />
-        {items.map(item => <Person key={item.name} person={item} />)}
+        <Button>button</Button>
+        <hr />
+        <LabelHOC>label</LabelHOC>
       </div>
     )
   }
 }
 
-const Person = (props) => <h4>{props.person.name}</h4>
+const Button = HigherOrderComponent((props) =>
+  <button
+    onClick={props.update}
+  >
+    {props.children} - {props.count}
+  </button>
+)
 
+class Label extends React.Component {
+  componentWillMount() {
+    console.log('label will mount')
+  }
+
+  render() {
+    return(
+      <label
+        onMouseMove={this.props.update}
+      >
+        {this.props.children} - {this.props.count}
+      </label>
+    )
+  }
+}
+
+const LabelHOC = HigherOrderComponent(Label)
 
 export default App;
